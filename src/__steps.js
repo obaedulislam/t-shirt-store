5e3a89a837ddd5d7e9a87d1e1bc04295678d8660
obaedulislam <obaed.cse@gmail.com>
2022-10-10 03:37:01 +0600

    Route Setup Done

---
+++ src/__steps.js
@@ -0,0 +1,13 @@
+/*
+    Basic Router Set-Up:
+    
+    1. npm i react-router-dom
+    2. create a router using createBrowserRouter
+    3. add RouterProvider and set router prop
+    4. Create a lot of components
+    5. Create Main Layout with some common part and one changing part based on the route
+    6. Set Main layout at the Root of the route
+    7. Set childreen routes
+    8. Set Links on the Header Component
+
+*/
